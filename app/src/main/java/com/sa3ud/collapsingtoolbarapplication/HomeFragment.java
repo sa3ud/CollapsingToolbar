171d2ea5bbee63d45abc0bd006c19faf0eba39d6
user <alshabibo@gmail.com>
2021-06-21 19:09:40 +0400

    final

---
+++ app/src/main/java/com/sa3ud/collapsingtoolbarapplication/HomeFragment.java
@@ -0,0 +1,32 @@
+package com.sa3ud.collapsingtoolbarapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class HomeFragment extends Fragment {
+
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View parentView = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+
+
+        return parentView;
+    }
+}
